00b7254d2a386a5b568c749224a05ed3b46ca67a
Taurese <43673192+25487@users.noreply.github.com>
2019-05-15 11:10:03 +0200

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,128 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+// hoofd
+context.beginPath();
+context.arc(200, 200, 150, 0, 2 * Math.PI);
+context.fillStyle = "rgb(127,63,255)";
+context.fill();
+context.closePath();
+context.stroke();
+// neus
+context.beginPath();
+context.arc(200, 180, 17.5, 0, 2 * Math.PI);
+context.fillStyle = "rgb(127,63,255)";
+context.fill();
+context.closePath();
+context.stroke();
+// mond
+context.beginPath();
+context.arc(200, 230, 50, 0, 1 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+// oog-1
+context.beginPath();
+context.arc(130, 130, 35, 0, 2 * Math.PI);
+context.fillStyle = "white";
+context.fill();
+context.closePath();
+context.stroke();
+// oog-2
+context.beginPath();
+context.arc(290, 130, 35, 0, 2 * Math.PI);
+context.fillStyle = "white";
+context.fill();
+context.closePath();
+context.stroke();
+// pupil-1
+context.beginPath();
+context.arc(130, 130, 20, 0, 2 * Math.PI);
+context.fillStyle = "brown";
+context.fill();
+context.closePath();
+context.stroke();
+// pupil-2
+context.beginPath();
+context.arc(290, 130, 20, 0, 2 * Math.PI);
+context.fillStyle = "brown";
+context.fill();
+context.closePath();
+context.stroke();
+//pupil-2
+context.beginPath();
+context.arc(130, 130, 10, 0, 2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+// pupil-3
+context.beginPath();
+context.arc(290, 130, 10, 0, 2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+//kin
+context.beginPath();
+context.arc(140, 310, 25, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+//kin
+context.beginPath();
+context.arc(170, 310, 25, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+//kin
+context.beginPath();
+context.arc(210, 310, 25, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+//kin
+context.beginPath();
+context.arc(80, 200, 40, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+context.beginPath();
+//kin
+context.arc(60, 200, 40, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+//kin
+context.beginPath();
+context.arc(250, 200, 40, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+context.beginPath();
+//kin
+context.arc(270, 200, 40, -0.5, 0.2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
+context.stroke();
+// oor-1
+context.beginPath();
+context.arc(60, 130, 20, 0, 2 * Math.PI);
+context.fillStyle = "rgb(127,63,255)";
+context.fill();
+context.closePath();
+context.stroke();
+// oor-2
+context.beginPath();
+context.arc(350, 130, 20, 0, 2 * Math.PI);
+context.fillStyle = "rgb(127,63,255)";
+context.fill();
+context.closePath();
+context.stroke();
